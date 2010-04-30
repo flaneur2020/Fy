0d593861dc0c95645b0b0b8cc7662f865c269b19
Fleurer <me.ssword@gmail.com>
2010-04-30 20:59:11 +0800

    remove the egg hurting code of categoryModel

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,24 +1,21 @@
 class Category < ActiveRecord::Base
-  acts_as_tree 
+  acts_as_tree :order => :created_at
 
   has_many :posts
   
   # validations
   validates_presence_of :name
   validates_uniqueness_of :name
+
+  
+
   # TODO: valid do not set parent as self
 
   # the roots' parents are nil
   # TODO: get depth when traverse
   # have a cache
   def depth
-    return @depth if @depth
-    r = 0
-    c = self
-    while c = c.parent
-      r += 1
-    end
-    return @depth = r
+    self.ancestors.size
   end 
 
   def Category.as_list(root=nil, r=[])
@@ -29,7 +26,7 @@ class Category < ActiveRecord::Base
       end
     else
       r = []
-      Category.find_roots.each do |c|
+      Category.roots.each do |c|
         Category.as_list(c, r)
       end
     end
@@ -41,10 +38,6 @@ class Category < ActiveRecord::Base
     return Category.find(*args)
   rescue
     return Category.new
-  end
-
-  def Category.find_roots
-    Category.find_all_by_parent_id(nil)
   end
 
 end
