ee1a35b22237fb4bdcbacbf73363a2ff950d930d
Fleurer <me.ssword@gmail.com>
2010-05-04 19:30:39 +0800

    prepare to upload... now just some css

---
+++ app/controllers/upload_controller.rb
@@ -0,0 +1,25 @@
+class UploadController < ApplicationController
+
+  def index
+    redirect_to :action => :add
+  end
+
+  def list
+  end
+
+  # add an image here
+  def add
+  end
+
+  # edit info on image
+  def edit
+  end
+
+  def save
+  end
+
+  def rm
+  end
+
+
+end
